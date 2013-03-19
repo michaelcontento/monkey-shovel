7ca5e7611a188f1f470e6102d599d8bf8a49f648
Michael Contento <michaelcontento@gmail.com>
2013-03-19 09:14:09 +0100

    added task pxm.export_dir()

--- pxm.py
+++ pxm.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, division, with_statement
 from sh import automator
 from shovel import task
 from meta.utils import path_data, path_meta
+from path import path
 
 WORKFLOW = path_data() / "pxm-export.workflow"
 
@@ -17,4 +18,14 @@ def export(filenames=None):
         filenames = path_meta().walk("*.pxm")
 
     for filename in filenames:
+        automator("-i", filename, WORKFLOW)
+
+
+@task
+def export_dir(dirname=None):
+    if dirname is None:
+        dirname = path_meta()
+
+    for filename in path(dirname).walk("*.pxm"):
+        print filename
         automator("-i", filename, WORKFLOW)
